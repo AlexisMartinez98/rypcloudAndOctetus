c94917711d1a2e3b8996c4ce0221804276bab2d6
Micaela <micaelauvilla9@gmail.com>
2024-09-13 16:14:21 -0300

    add component services

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/NavBar";
 import About from "./components/About";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
       <NavBar />
       <About/>
+      <Services/>
     </>
   );
 }
